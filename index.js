13dfabefa8418d293934fdffed4cd38b4378290a
noboKumar <nobokumar227@gmail.com>
2025-05-21 14:45:32 +0600

    get active user from db

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.bmuc12j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const activeUsersCollection = client
+      .db("gardenNestDB")
+      .collection("activeUsers");
+
+    app.get("/users", async (req, res) => {
+      const result = await activeUsersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/activeUsers", async (req, res) => {
+      const result = await activeUsersCollection
+        .find({ status: "Active" })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("server is running");
